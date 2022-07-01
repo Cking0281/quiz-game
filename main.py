8a3f740775dfc5507b7ad7e204978022b33fb25d
Cody King <cody.king04@outlook.com>
2022-07-01 09:20:12 -0400

    Added quizbrain class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 
 def main():
@@ -8,6 +9,9 @@ def main():
         question_text = question["text"]
         question_answer = question["answer"]
         question_bank.append(Question(question_text, question_answer))
+
+    quiz_brain = QuizBrain(question_bank)
+    quiz_brain.next_question()
 
 
 if __name__ == "__main__":
